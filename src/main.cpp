46a47c65288581020d443a2a0cff7aaaaa465b21
Niel Cansino <niel.cansino@ali.jp>
2020-05-05 18:12:13 +0900

    added characteristic

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,46 @@
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC2_UUID "29efd687-7f8c-4a6e-9e50-c61f42d6361d"
 
 void setup() {
+
+  // Begin serial debug port
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
+  // it seems that the device doesn't have to be instantiated here
+  // Library is calling a static function
   BLEDevice::init("Long name works now");
+
+  // Create bluetooth server (advertiser)
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a characteristic with a UUID defined by CHARACTERISTIC_UUID
+  // inside the service pService
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
+  // Create a characteristic with a UUID defined by CHARACTERISTIC2_UUID
+  // inside the service pService
+  BLECharacteristic *pCharacteristic2 = pService->createCharacteristic(
+                                         CHARACTERISTIC2_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  // Sets the value for CHARACTERISTIC
   pCharacteristic->setValue("Hello World says Neil");
+  
+  // Sets the value and descriptor for CHARACTERISTIC2
+  pCharacteristic2->setValue("This is Niel... hehehe");
+  pCharacteristic2->setNotifyProperty(true);
+ 
+  // Start the service
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
